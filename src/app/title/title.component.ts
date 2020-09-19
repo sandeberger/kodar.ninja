b4a4cfcaaa42e482d82954b9b724211e8c2eddc4
Joakim Sandeberger <joakim.sandeberger@gmail.com>
2020-09-19 23:31:35 +0200

    Styling & Fonts

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styles: [
-  ]
+  styleUrls: ['./title.component.scss'],
 })
 export class TitleComponent implements OnInit {
 
