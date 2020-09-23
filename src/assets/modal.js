20b532ebb42efc6ba75e8f0724c8518c7c4c3fe9
Joakim Sandeberger <joakim.sandeberger@gmail.com>
2020-09-23 14:22:56 +0200

    Modals, buttons, favicon, styles

---
+++ src/assets/modal.js
@@ -0,0 +1,60 @@
+const showDialog = (id) => {
+  document.getElementById("dialog").classList.add('show')
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+    
+
+/***/
+
+    google.charts.load('current', {
+          packages:['timeline', 'table']
+        }).then(function () {
+          //var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1bIydJJY_-H9NHHhxca9U-jNmKjYUkln14v20N7klAGg/edit?usp=sharing');
+          var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1gkyETRRwrCv3ats74cbgGXFp13ABtkkdKS7EcJ10waw/edit?usp=sharing');
+          query.setQuery("select B,C,D where A like '"+id+"'");
+          //query.setQuery('select E,F,A,C');
+          query.send(handleQueryResponse);
+
+          function handleQueryResponse(response) {
+            if (response.isError()) {
+              console.log('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+              return;
+            }
+
+            var container = document.getElementById('timeline');
+            var chart = new google.visualization.Table(container);
+              
+            var cssClassNames = {
+                'headerRow': 'head-background',
+                'tableRow': 'row-background',
+                'headerCell': 'head-background',
+                'tableCell': '',
+                'headerCell': 'head-background',
+                'rowNumberCell': 'head-background'};              
+            var options = {'showRowNumber': false, 'allowHtml': true, alternatingRowStyle: false, page: 'enable', pageSize: '10', pagingtype: 'simple', title: 'Projekt','cssClassNames': cssClassNames};              
+              
+            var dataTable = response.getDataTable();              
+            chart.draw(dataTable, options);
+
+/*google.visualization.events.addListener(chart, 'select', function() {
+    var row = chart.getSelection()[0].row;
+    alert('You selected ' + dataTable.getValue(row, 0));
+  });              */
+              
+          }
+        });
+    
+/***/
+    
+};
+const closeDialog = () => {
+  const body = document.body;
+  //const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  //window.scrollTo(0, parseInt(scrollY || '0') * -1);
+  document.getElementById('dialog').classList.remove('show');
+}
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
