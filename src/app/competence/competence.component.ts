5b7f1a26bb0fdb204ecbfc62da70ceb701053bb1
Joakim Sandeberger <joakim.sandeberger@gmail.com>
2020-09-29 19:45:56 +0200

    test

--- src/app/competence/competence.component.ts
+++ src/app/competence/competence.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-competence',
   templateUrl: './competence.component.html',
-  styles: [
-  ]
+  styleUrls: ['./competence.component.scss']
 })
 export class CompetenceComponent implements OnInit {
 
